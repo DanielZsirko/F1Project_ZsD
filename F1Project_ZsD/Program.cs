b6390352a22c5371e919f4c1b82037a004edaac0
Dániel <zsirko.daniel.attila@blathy.info>
2023-12-29 12:04:45 +0100

    Structure datatags defined

--- F1Project_ZsD/Program.cs
+++ F1Project_ZsD/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿struct F1Racing
+{
+    public int seasonnum;
+    public string winnerpilot;
+    public string winnerteam;
+    public int numofraces;
+    public int numofracers;
+    public int numofteams;
+}
