cd8c325cbc1345bbe22073c2d51a775796728044
Dániel <zsirko.daniel.attila@blathy.info>
2023-12-30 12:28:40 +0100

    Structure constructor defined

--- F1Project_ZsD/Program.cs
+++ F1Project_ZsD/Program.cs
@@ -6,4 +6,15 @@
     public int numofraces;
     public int numofracers;
     public int numofteams;
+
+    public F1Racing(string line)
+    {
+        string[] splitted = line.Split(';');
+        this.seasonnum = int.Parse(splitted[0]);
+        this.winnerpilot = splitted[1];
+        this.winnerteam = splitted[2];
+        this.numofraces = int.Parse(splitted[3]);
+        this.numofracers = int.Parse(splitted[4]);
+        this.numofteams = int.Parse(splitted[5]);
+    }
 }
